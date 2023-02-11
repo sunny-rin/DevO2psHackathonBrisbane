eee29d3174efb7c46081e8c5de0c0cadff2a4301
laila-rin <56868999+laila-rin@users.noreply.github.com>
2023-02-11 05:41:54 +0100

    add comment

--- Application/src/RazorPagesTestSample/Data/AppDbContext.cs
+++ Application/src/RazorPagesTestSample/Data/AppDbContext.cs
@@ -2,10 +2,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+//comment hi
 
 namespace RazorPagesTestSample.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : DbContext 
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) 
             : base(options)
